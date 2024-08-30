f0dc98c0ce4ac350492242bd2b8a19ec88774fca
DZhang3210 <davidzhang3210@gmail.com>
2024-08-30 17:06:02 -0400

    new commit

---
+++ src/app/api/[storeId]/checkout/route.ts
@@ -0,0 +1,83 @@
+import Stripe from "stripe";
+
+import { NextResponse } from "next/server";
+import { db } from "@/server/db";
+import { stripe } from "@/lib/stripe";
+
+const corsHeader = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeader });
+}
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } },
+) {
+  const { productIds } = await req.json();
+  if (!productIds || productIds.length === 0) {
+    return new NextResponse("Product ids are required", { status: 400 });
+  }
+
+  const items = await db.product.findMany({
+    where: {
+      id: {
+        in: productIds,
+      },
+    },
+  });
+
+  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = items.map(
+    (item) => {
+      return {
+        quantity: 1,
+        price_data: {
+          currency: "USD",
+
+          product_data: {
+            name: item.name,
+          },
+          unit_amount: item.price.toNumber() * 100,
+        },
+      };
+    },
+  );
+
+  const order = await db.order.create({
+    data: {
+      storeId: params.storeId,
+      isPaid: false,
+      orderItems: {
+        create: productIds.map((productId: string) => ({
+          product: {
+            connect: {
+              id: productId,
+            },
+          },
+        })),
+      },
+    },
+  });
+
+  const stripeSession = await stripe.checkout.sessions.create({
+    success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
+    cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
+    payment_method_types: ["card"],
+    mode: "payment",
+    billing_address_collection: "required",
+    phone_number_collection: {
+      enabled: true,
+    },
+    // customer_email: user.emailAddresses[0]?.emailAddress,
+    line_items: line_items,
+    metadata: {
+      orderId: order.id,
+    },
+  });
+
+  return NextResponse.json({ url: stripeSession.url }, { headers: corsHeader });
+}
---
+++ src/app/api/webhook/stripe/route.ts
@@ -0,0 +1,69 @@
+import { stripe } from "@/lib/stripe";
+import { db } from "@/server/db";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+export async function POST(req: Request) {
+  const body = await req.text();
+  const signature = headers().get("Stripe-Signature") as string;
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!,
+    );
+  } catch (err: any) {
+    return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 });
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+  const address = session?.customer_details?.address;
+
+  const addressComponents = [
+    address?.line1,
+    address?.line2,
+    address?.city,
+    address?.state,
+    address?.postal_code,
+    address?.country,
+  ];
+
+  const addressString = addressComponents.filter((c) => c !== null).join(", ");
+
+  if (event.type === "checkout.session.completed") {
+    if (!session?.metadata?.userId) {
+      return new NextResponse("User id is required", { status: 400 });
+    }
+    const order = await db.order.update({
+      where: {
+        id: session?.metadata.userId,
+      },
+      data: {
+        isPaid: true,
+        address: addressString,
+        phone: session?.customer_details?.phone || "",
+      },
+      include: {
+        orderItems: true,
+      },
+    });
+
+    const productIds = order.orderItems.map((orderItem) => orderItem.productId);
+
+    await db.product.updateMany({
+      where: {
+        id: {
+          in: [...productIds],
+        },
+      },
+      data: {
+        isArchived: true,
+      },
+    });
+  }
+  return new NextResponse(null, { status: 200 });
+}
